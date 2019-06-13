4970316d757949ceb8d649872315d19e58a702af
Carl Sandström <carl@atmax.se>
2019-06-13 10:55:31 +0200

    Make file change to solve 'undefined reference to _binary_f...' build errors

--- websocket_server/main/component.mk
+++ websocket_server/main/component.mk
@@ -1,1 +1,1 @@
-COMPONENT_PRIV_INCLUDEDIRS := server
+COMPONENT_EMBED_FILES := ./server/error.html ./server/favicon.ico ./server/root.html ./server/test.css ./server/test.js
--- websocket_server/main/server/component.mk
+++
@@ -1,2 +0,0 @@
-# files to add to the web server
-COMPONENT_EMBED_FILES := root.html error.html test.js test.css favicon.ico
